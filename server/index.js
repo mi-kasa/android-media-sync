c09884da8ab0b7255cc598e88e700dc76c2323de
Francisco Jordano <fjordano@mozilla.com>
2016-11-18 17:46:08 +0000

    Modularising the upload method

--- server/index.js
+++ server/index.js
@@ -1,47 +1,38 @@
-const express = require('express')
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
-const jsonfile = require('jsonfile')
-const path = require('path')
-const fs = require('fs-extra')
-const debug = require('debug')('upload', true)
+const express = require('express');
+const multer  = require('multer');
+const fs = require('./lib/fs');
+fs.ensureDefaultUploadDirectory();
+const upload = multer({ dest: fs.getDefaultUploadDirectory() });
+const debug = require('debug')('upload');
 
-const app = express()
+const app = express();
 
 app.post('/upload', upload.array('image', 1), function(req, res, next) {
-  const file = req.files[0]
-  debug('Got new file to upload ', file.originalname);
-  const filePath = file.path
-  const destination = path.join(file.destination, file.originalname)
-  debug('Destination ', destination);
-  var metadata = Object.assign({}, req.file)
-  metadata = Object.assign(metadata, req.body)
-  debug('Metadata ', metadata);
+  const file = req.files[0];
+  var metadata = Object.assign({}, file);
+  metadata = Object.assign(metadata, req.body);
 
-  fs.move(filePath, destination, (err) => {
-    if (err) {
-      debug("Error while moving file ", err);
-      return res.status(500).send(err)
-    }
-
-    jsonfile.writeFile(`uploads/${metadata.title}.json`, metadata, (err) => {
-      debug(`File ${metadata.title} uploaded correctly`)
-      res.send("OK")
-    })
-
-  });
-})
+  fs.postProcessUpload(metadata).
+    then(() => {
+      debug("Upload finished ok");
+      res.send("OK");
+    }).
+    catch(reason => {
+      debug.error(reason);
+      res.status(500).send(reason);
+    });
+});
 
 app.get('/ping', function(req, res, next) {
-  debug('Sending pong')
-  res.send("pong")
-})
+  debug('Sending pong');
+  res.send("pong");
+});
 
 app.use(function (err, req, res, next) {
-  debug(err.stack)
-  res.status(500).send(err)
-})
+  debug(err.stack);
+  res.status(500).send(err);
+});
 
-app.listen(3000, '192.168.0.15', function () {
-  debug('Upload server listening on port 3000!')
-})
+app.listen(3000, '10.239.30.165', function () {
+  debug('Upload server listening on port 3000!');
+});
